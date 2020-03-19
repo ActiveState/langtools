2f6aaf5d6c95e67231bbc718af34a921ca068dcb
Dave Rolsky <autarch@urth.org>
2020-03-19 16:32:07 -0500

    Fix incorrect link in comment

--- pkg/name/python.go
+++ pkg/name/python.go
@@ -11,8 +11,8 @@ var replacement = regexp.MustCompile(`[\.\_-]+`)
 // NormalizePython takes a Python package name and returns it in normalized
 // form. Specifically, that means it is in all lower case and all periods (.)
 // and underscores (_) with hyphens. See
-// https://www.python.org/dev/peps/pep-0503/#name for details on how names
-// should be normalized in Python.
+// https://www.python.org/dev/peps/pep-0503/#normalized-names for details on
+// how names should be normalized in Python.
 func NormalizePython(name string) string {
 	return strings.ToLower(replacement.ReplaceAllString(name, "-"))
 }
